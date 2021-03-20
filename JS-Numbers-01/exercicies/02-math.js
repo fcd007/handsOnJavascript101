31328e045e9e937f9867901d64ffbfdee7ef3324
Claudeilton Dantas <fcd007@hotmail.com>
2021-03-20 19:52:33 -0300

    chore: add files Math tests e results

---
+++ JS-Numbers-01/exercicies/02-math.js
@@ -0,0 +1,19 @@
+let numero = 9.8167
+console.log(numero);
+//arrendodamento para baixo valores 
+let numero2 = Math.floor(numero)
+console.log(numero2);
+//arrendondar para cima
+let numero3 = Math.ceil(numero)
+console.log(numero3);
+//arrendondar para o mais valor próximo
+console.log(Math.round(numero));
+//máximo número de uma sequência
+console.log(Math.max(1,10,574,255,78));
+console.log(Math.min(1,10,574,255,-78));
+console.log(Math.random() * (10 - 5) + 5);
+console.log(Math.PI);
+console.log(Math.pow(7,2));
+
+//problema de dividir por zero - isso é avaliado como true
+console.log(100 / 0.000000001);
