a37f56ad89b12e195fe8030bcec140c6725a2858
Claudeilton Dantas <fcd007@hotmail.com>
2021-03-20 20:04:17 -0300

    chore: add files Math tests e results

---
+++ JS-Numbers-01/exercicies/pratical-numbers-math.js
@@ -0,0 +1,8 @@
+let numero = 49
+console.log(`Seu número é ${numero}`)
+console.log(`Raiz quadrada: ${Math.sqrt(numero)}`)
+console.log(`${numero} é inteiro ${Number.isInteger(numero)}`)
+console.log(`É NaN ${Number.isNaN(numero)}`);
+console.log(`Arrendando para baixo ${Math.floor(numero)}`);
+console.log(`Arrendando para cima ${Math.ceil(numero)}`);
+console.log(`Com duas casas decimais: ${numero.toFixed(2)}`);
