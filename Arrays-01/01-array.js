7271c7893a841614e1bfd4c962ed8671705fce9e
Claudeilton Dantas <fcd007@hotmail.com>
2021-03-22 08:38:57 -0300

    chore: tests arrays and objects js

---
+++ Arrays-01/01-array.js
@@ -0,0 +1,15 @@
+let names = ['Carlos', 'Lucas', 'Maria', 'Paulo', 'Juliana']
+//exibir elementos do array via loop for
+// for (const name of names) {
+//   console.log(name);
+// }
+//exibindo lista de names
+console.log(names);
+//exibindo os elementos pelo indice do array
+console.log(names[0]);
+console.log(names[1]);
+console.log(names[2]);
+console.log(names[3]);
+console.log(names[4]);
+//não temos elemento no indice 5 que equivale ao 6º elemento
+console.log(names[5]); // undefined
