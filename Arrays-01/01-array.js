6f53b2ce3c43583d03315e718b3e5daf30cb4399
Claudeilton Dantas <fcd007@hotmail.com>
2021-03-22 08:56:20 -0300

    chore: add variables tests resources Arrays

--- Arrays-01/01-array.js
+++ Arrays-01/01-array.js
@@ -1,10 +1,11 @@
+
 let names = ['Carlos', 'Lucas', 'Maria', 'Paulo', 'Juliana']
 //exibir elementos do array via loop for
 // for (const name of names) {
 //   console.log(name);
 // }
 //exibindo lista de names
-console.log(names);
+// console.log(names);
 //exibindo os elementos pelo indice do array
 console.log(names[0]);
 console.log(names[1]);
@@ -12,4 +13,32 @@ console.log(names[2]);
 console.log(names[3]);
 console.log(names[4]);
 //não temos elemento no indice 5 que equivale ao 6º elemento
-console.log(names[5]); // undefined
+// console.log(names[5]); // undefined
+// add elementos no array
+// names[5] = 'Lisa'
+// console.log(names);
+//saber o tamanho do array
+// console.log(`Array possui ${names.length} elementos`);
+//adicionando com métodos do array push ao final do array
+// names.push('Lilian')
+// console.log(names)
+// //adicionando elementos no inicio do array
+// names.unshift('Fabiana')
+// console.log(names)
+// names.unshift('Luana')
+// console.log(names)
+//remove elementos do fim do array
+const element = names.pop()
+console.log(names);
+console.log(element)
+//apagando elementos pelo indice
+// delete names[1] //elimina o valor, não o elemento do array
+// names.pop()
+// console.log(names);
+
+//fatiando os elementos por faixa de indices
+// console.log(names.slice(1,3));
+// console.log(names.slice(1,-1));
+//verificar o tipo de dado usado 
+console.log(typeof names);
+console.log(names instanceof Array);
